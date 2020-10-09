ee4a57cba737b77367f0d3a4a62389ac4802e7ea
Danny McCracken <dannymccracken13@yahoo.com>
2020-10-09 14:21:28 -0500

    Created three functions for separating holy fish, soldier fish, and regular fish

--- scripts/Fish/FishDataProvider.js
+++ scripts/Fish/FishDataProvider.js
@@ -25,6 +25,39 @@ const fishCollection = [
         image: "https://www.livetropicalfish.net/wp-content/uploads/2018/11/Emperor-angelfish.jpg"
     }
 ]
-export const useFish = () => {
-    return fishCollection.slice()
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = []
+
+    for (const fish of fishCollection) {
+        if (fish.length % 3 === 0) {
+            holyFish.push(fish)
+        }
+    }
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const soldiers = []
+
+    for (const fish of fishCollection) {
+        if(fish.length %5 === 0 && fish.length % 3 !== 0) {
+            soldiers.push(fish)
+        }
+    }    
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+   const regularFish = []
+
+   for (const fish of fishCollection) {
+       if(fish.length %3 !== 0 && fish.length % 3 !== 0) {
+           regularFish.push(fish)
+       }
+   }
+    return regularFish
 }
